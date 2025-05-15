4e41fb3b5709f7ceedf052d6e61e9d5a91170a74
Wasim <starkxarc@gmail.com>
2025-05-15 22:26:01 +0530

    2025-05-15 22:26:01

--- test-random-question-answer-chat.py
+++ test-random-question-answer-chat.py
@@ -9,7 +9,7 @@ from langdetect import detect
 import random
 import time
 
-# Set your API keyss
+# Set your API keys
 openai.api_key = "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"
 WEATHER_API_KEY = "64f71b95aec8db14e72121d04e040220"
 

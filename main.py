3c9d4d80b3460342673f8b6041ede9c3685a4dc3
Wasim <starkxarc@gmail.com>
2025-05-23 00:02:45 +0530

    2025-05-23 00:02:45

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from gtts import gTTS
 import threading
 
 # Set your API keys here
-openai.api_key = "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"
-WEATHER_API_KEY = "64f71b95aec8db14e72121d04e040220"
+openai.api_key = "API_KEY"
+WEATHER_API_KEY = "API_KEY"
 
 recognizer = sr.Recognizer()
 
